8f2cbd594d1b0244f4966a6e73bd291b2d3e146e
chinotapales <carlo.tapales@coreproc.ph>
2018-08-27 12:34:38 +0800

    Adjusted Table View Constraints So Forecast Items Do Not Clip

--- SimpleWeatherForecast/app/ui/presentation/custom/cells/day/day_forecast/DayForecastTableViewCell.swift
+++ SimpleWeatherForecast/app/ui/presentation/custom/cells/day/day_forecast/DayForecastTableViewCell.swift
@@ -73,7 +73,7 @@ extension DayForecastTableViewCell: UICollectionViewDelegate, UICollectionViewDe
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        return UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
     }
     
 }
