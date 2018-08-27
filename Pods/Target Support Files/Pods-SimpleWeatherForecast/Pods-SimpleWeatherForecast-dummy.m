2308c4cf6faf23e480abd6b8681dc07e77b5dd32
chinotapales <carlo.tapales@coreproc.ph>
2018-08-27 18:55:10 +0800

    Deintegrated Pods for Merging

--- Pods/Target Support Files/Pods-SimpleWeatherForecast/Pods-SimpleWeatherForecast-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimpleWeatherForecast : NSObject
-@end
-@implementation PodsDummy_Pods_SimpleWeatherForecast
-@end
--- Pods/Target Support Files/Pods-SimpleWeatherForecastTests/Pods-SimpleWeatherForecastTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimpleWeatherForecastTests : NSObject
-@end
-@implementation PodsDummy_Pods_SimpleWeatherForecastTests
-@end
